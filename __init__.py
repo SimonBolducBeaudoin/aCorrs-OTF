9a8c2edfbed221468a44dee4b96c354c8b2f8a8e
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-02 12:47:52 -0400

    init file update

--- __init__.py
+++ __init__.py
@@ -2,14 +2,8 @@
 #! -*- coding: utf-8 -*-
 
 import os as _os, platform as _platform
-mingw_path = 'C:\\cygwin64\\usr\\x86_64-w64-mingw32\\sys-root\\mingw\\bin'
-if _os.name == 'nt':
-    if _platform.python_version_tuple()[0]== '2' : #python 2
-        if ( mingw_path not in _os.environ['PATH'] ) :
-            _os.environ['PATH'] = mingw_path+_os.path.pathsep+_os.environ['PATH']
-    else : # python 3      
-        _os.add_dll_directory("C:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
+
+from SBB.Pyhegel_extra.Env_setup import add_Cygwin_mingw_to_path
+add_Cygwin_mingw_to_path()
 
 __all__ = ["util","acorrs_otf"]
-
-del mingw_path
